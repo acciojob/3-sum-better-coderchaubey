73227f8d20a357955d8c863cc56dc07f9d914cdb
coderchaubey <115737868+coderchaubey@users.noreply.github.com>
2023-04-25 22:12:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function threeSum(arr, target) {
+function threeSum(S, target) {
 // write your code here
-  
+  for (let index = 0; index < S.length; index++) {
+	  let j=index+1;
+  	for (let i = index; i < s.length; i++) {
+  		if (S[index]+S[j]+S[i]===target || S[index]+S[j]+S[i]===target-1 || S[index]+S[j]+S[i]===target+1) {
+  			return S[index]+S[j]+S[i];
+  		}
+  	}
+  }
 }
 
 module.exports = threeSum;
